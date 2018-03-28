dbc00fe5de09a3408a5b959e49161e6c7f1a6b20
Austin-Olbrych <35496252+Austin-Olbrych@users.noreply.github.com>
2018-03-27 22:59:15 -0400

    Settings Commit

---
+++ Settings/Notifications/app/src/main/java/com/example/twins/notifications/NotificationActivity.java
@@ -0,0 +1,18 @@
+package com.example.twins.notifications;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Twins on 3/4/2018.
+ */
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+
+}
