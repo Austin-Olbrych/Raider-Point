32773ac869e4e45ef259cac7933567220781bd06
Austin-Olbrych <35496252+Austin-Olbrych@users.noreply.github.com>
2018-04-13 09:07:59 -0400

    Added Intent

--- DetailActivity.java
+++ DetailActivity.java
@@ -1,62 +1,92 @@
-<?xml version="1.0" encoding="utf-8"?>
-<android.support.constraint.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context="c.scrummies.detailview.DetailView">
+package c.scrummies.detailview;
 
-    <TextView
-        android:id="@+id/txtName"
-        android:layout_width="260dp"
-        android:layout_height="51dp"
-        android:padding="5dp"
-        android:text="@string/event_name"
-        android:textAlignment="center"
-        android:textSize="28sp"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toTopOf="parent"
-        app:layout_constraintVertical_bias="0.191" />
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
-    <TextView
-        android:id="@+id/txtDate"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:padding="5dp"
-        android:text="@string/event_date"
-        android:textAlignment="center"
-        android:textSize="18sp"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintHorizontal_bias="0.501"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toTopOf="parent"
-        app:layout_constraintVertical_bias="0.351" />
+import java.util.Calendar;
 
-    <TextView
-        android:id="@+id/txtLocation"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:padding="5dp"
-        android:text="@string/event_loc"
-        android:textSize="18sp"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toTopOf="parent" />
+public class DetailView extends AppCompatActivity {
 
-    <Button
-        android:id="@+id/btnCalendar"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="@string/btn_cal"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintHorizontal_bias="0.5"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toBottomOf="@+id/txtLocation" />
+    int year,month,date,hour,minute; //Variables for start and end time
+    String name,location; //Variables for name and location
+    long startMillis = 0;
+    long endMillis = 0;
 
+    /* For Hailey
 
-</android.support.constraint.ConstraintLayout>
+    //What is needed
+    public void openDetailView (View view) {
+        Intent intent = new Intent(this, DetailView.class);
+        intent.putExtra("eventID", variable);
+        startActivity(intent);
+    }
+    //Example 1 starting intent
+    public void openDetailView (View view) {
+        Intent intent = new Intent(this, DetailView.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    Example 2 variable through intent
+    Intent intent = new Intent(MainActivity.this, DetailView.class);
+    intent.putExtra("eventID", variable);
+    startActivity(intent);
+
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_detail_view);
+
+        //Read in variable from MainActivity
+        //Intent activityIntent = getIntent();
+        //int eventID = activityIntent.getDoubleExtra("eventID", 0);
+
+        Button btnAdd = (Button) findViewById(R.id.btnCalendar);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                insert();
+            }
+        });
+    }
+
+    @SuppressLint("NewApi")
+    public void insert() {
+        Calendar cal = Calendar.getInstance();
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+
+        intent.putExtra("title", "Name of Event"); //Event title
+        intent.putExtra("eventLocation", "KHIC"); //Event location
+
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2018, 0, 22, 17, 0);
+        startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2018, 0, 22, 18, 45);
+        endMillis = endTime.getTimeInMillis();
+
+        intent.putExtra("beginTime", startMillis);
+        intent.putExtra("endTime", endMillis);
+
+        startActivity(intent);
+    }
+}
